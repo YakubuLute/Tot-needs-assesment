4d323bf30e0ec18edbbcecf81ffaf1ced2889f2d
Yakubu Lute <yakubulute@outlook.com>
2021-10-24 09:04:07 -1200

    Update bookmarkcontroller.dart

--- lib/controller/bookmarkcontroller.dart
+++ lib/controller/bookmarkcontroller.dart
@@ -1,44 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/model/recipe_model.dart';
 import 'package:recipe_app/service/bookmark_recipe.dart';
+import 'package:flutter/foundation.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class BookMarkController {
-  BookmarkService bookMarkService = BookmarkService();
+class BookMarkController with ChangeNotifier {
+  //constructor
+  BookMarkController();
 
-  Future<List<RecipeModel>> getUserBookMarkRecipe() async {
+  BookmarkService bookmarkService = BookmarkService();
+
+  Future<List<RecipeModel>?> getAllBookMarks() async {
     try {
-      await bookMarkService.open();
-      var bookmark = await bookMarkService.getAllRecipe();
-      if (bookmark != null) {
-        await bookMarkService.close();
+      await bookmarkService.open();
+      List<RecipeModel>? recipies = await bookmarkService.getAllRecipe();
+      if (recipies != null) {
+        notifyListeners();
+        await bookmarkService.close();
+        return recipies;
       }
+      return null;
     } catch (error) {
-      //if error
-      print("Something went wrong $error");
-    }
-    await bookMarkService.close();
-    return [];
-  }
-
-  addBookMark(RecipeModel recipeModel) async {
-    try {
-      await bookMarkService.open();
-      bookMarkService.insert(recipeModel);
-      await bookMarkService.close();
-    } catch (error) {
-      //if error
-      print("Something went wrong $error");
+      await bookmarkService.close();
+      print("Sorry, Something went wrong $error");
+      return throw error;
     }
   }
 
-  deleteBookMark(id) async {
+  Future<bool> isBookmarked(RecipeModel recipeModel) async {
     try {
-      await bookMarkService.open();
-      bookMarkService.delete(id);
-      await bookMarkService.close();
+      await bookmarkService.open();
+      RecipeModel? recipies = await bookmarkService.getRecipe(recipeModel.id!);
+      notifyListeners();
+      await bookmarkService.close();
+      return recipies != null;
     } catch (error) {
-      //if error
-      print("Something went wrong $error");
+      await bookmarkService.close();
+      print("Something went wrong checking isBookmarked recipe $error");
+      throw error;
     }
   }
+
+  Future<RecipeModel?> addBookMark(RecipeModel recipeModel) async {
+    try {
+      await bookmarkService.open();
+      RecipeModel recipe = await bookmarkService.insert(recipeModel);
+      print('Added to boomarks: ${recipe.toJson()}');
+      await getAllBookMarks();
+      toast(msg: "Added to bookmarks");
+      await bookmarkService.close();
+
+      return recipe;
+    } catch (error) {
+      await bookmarkService.close();
+      print("Something went wrong adding recipies $error");
+      return null;
+    }
+  }
+
+  Future<int> deleteBookMark(RecipeModel recipeModel) async {
+    try {
+      await bookmarkService.open();
+      int deleteCount = await bookmarkService.delete(recipeModel.id!);
+      print('Deleted $deleteCount recipies from boomarks');
+      await getAllBookMarks();
+      toast(msg: "Removed from bookmarks");
+      await bookmarkService.close();
+      return deleteCount;
+    } catch (error) {
+      await bookmarkService.close();
+      print("Sorry something went wrong adding recipies $error");
+      return 0;
+    }
+  }
+}
+
+void toast({required String msg}) {
+  Fluttertoast.showToast(
+      msg: msg,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      fontSize: 16.0);
 }
